857648e195767af57343e97270379363d53a8e23
Mottaqui Karim <taqqui.karim@gmail.com>
2019-03-20 20:51:08 -0400

    using heroku provided port if available

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/ping', (req, res) => {
     res.json({'pong': '🏓'});
 });
 
-const port = 5011;
+const port = process.env.PORT || 5011;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 })
